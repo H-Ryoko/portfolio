876756755668cecece3a2f51214743a106b54502
H-Ryoko <94098568+H-Ryoko@users.noreply.github.com>
2021-11-20 17:25:12 +0900

    top(skill)/worklist/about(strong)のアニメーション実装

--- assets/js/global.js
+++ assets/js/global.js
@@ -7,3 +7,56 @@ $('a[href^="#"]').click(function(){
     $('body,html').animate({scrollTop:position}, speed, 'swing');
     return false;
 });
+function delayScrollAnime(){
+    var time = 0.3;
+    var value = time;
+    $('.section-skill__cards').each(function(){
+        var parent = this;
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var childs = $(this).children();
+
+        if(scroll >= elemPos - windowHeight && !$(parent).hasClass("play")){
+            $(childs).each(function(){
+
+                if(!$(this).hasClass("fadeUp")){
+
+                    $(parent).addClass("play");
+                    $(this).css("animation-delay", value + "s");
+                    $(this).addClass("fadeUp");
+                    value = value + time;
+
+                    var index = $(childs).index(this);
+                    if((childs.length-1) == index){
+                        $(parent).removeClass("play");
+                    }
+                }
+            })
+        }else{
+            $(childs).removeClass("fadeUp");
+            value = time;
+        }
+    })
+}
+$(window).scroll(function(){
+    delayScrollAnime();
+});
+
+function fadeInAnime(){
+    $('.section-strong').each(function(){
+        var elemPos = $(this).offset().top-50;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight){
+            $(this).addClass('fadeIn');// 画面内に入ったらfadeInというクラス名を追記
+        }else{
+            $(this).removeClass('fadeIn');// 画面外に出たらfadeInというクラス名を外す
+        }
+    });
+}
+
+$(window).scroll(function(){
+    fadeInAnime();
+});
+
